29195838243ab25e6303f5a4275ed540b529ba07
seeljack <seeljack@umich.edu>
2023-06-28 20:07:46 -0400

    JACK

--- Screens/ScreeningHistory.js
+++ Screens/ScreeningHistory.js
@@ -2,43 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
 import React from 'react';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
+import {AsyncStorage} from 'react-native';
 
 
-const getDataFromSurvey = () => {
-    // Create the data payload to send
-    const payload = {
-      the_data,
-    };
 
-    // Make an HTTP POST request to the "ScreeningHistory" page's URL
-    fetch('http://35.1.47.96:19000/screening-history', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-    return the_data
-    .then((response) => {
-      // Handle the response if needed
-      console.log('Data sent to ScreeningHistory');
-    })
-    .catch((error) => {
-      // Handle the error if any
-      console.error('Error sending data to ScreeningHistory:', error);
-    });
 
-  }
 
 const ScreeningHistory = ({navigation, route}) =>{    
 
+  _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem(route.params.the_data_string);
+      if (value !== null) {
+        // We have data!!
+        const parsedData = JSON.parse(value);
+        setData(parsedData);
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+    return value;
+  };
    
     return (
         <View>
             <Text>History of Past Screenings</Text>
             <View style={styles.allbuttons}>
                 <View style={styles.buttonstyle}>
-                    <Text>{getDataFromSurvey}</Text>
+                    <Text>{_retrieveData()}</Text>
                 </View>
                 <View style={styles.buttonstyle}>
                     <Pressable onPress={() => navigation.navigate('Hub')}>
--- Screens/SurveyPages/SurveyQ6.js
+++ Screens/SurveyPages/SurveyQ6.js
@@ -3,35 +3,26 @@ import { StyleSheet, Text, View, Image, Button, TextInput} from 'react-native';
 import React from 'react';
 import { SelectList } from 'react-native-dropdown-select-list'
 import { MultipleSelectList } from 'react-native-dropdown-select-list'
+import {AsyncStorage} from 'react-native';
+
+
 
 const SurveyQ6 = ({navigation, route}) =>{
   const [categories, setCategories] = React.useState([]);
   let {the_data} = route.params;
 
-  const sendDataToScreeningHistory = () => {
-    // Create the data payload to send
-    const payload = {
-      the_data,
-    };
 
-    // Make an HTTP POST request to the "ScreeningHistory" page's URL
-    fetch('http://35.1.47.96:19000/screening-history', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-    .then((response) => {
-      // Handle the response if needed
-      console.log('Data sent to ScreeningHistory');
-    })
-    .catch((error) => {
-      // Handle the error if any
-      console.error('Error sending data to ScreeningHistory:', error);
-    });
+  _storeData = async () => {
+    the_data_string = the_data.toString()
+    try {
+      await AsyncStorage.setItem(
+        the_data_string, 'I like to save it.'
+      );
+    } catch (error) {
+      // Error saving data
+    }
+  };
 
-  }
   return(
     <View style={styles.container}>
       <Text style={styles.textQ1}>
@@ -53,7 +44,7 @@ const SurveyQ6 = ({navigation, route}) =>{
               color="black"
               onPress={() => {
                 the_data.push(categories);
-                sendDataToScreeningHistory();
+                _storeData();
                 navigation.navigate('Hub', {the_data: the_data})
               }}
             />
